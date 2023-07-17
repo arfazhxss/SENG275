93023a55730d3977022bb2c47746ba1fc57b9a03
arfazhxss <arfazhussain@uvic.ca>
2023-07-17 00:25:18 -0700

    Routine Commit Commit by @arfazhxss on Mon 17 Jul 2023

--- route.sh
+++ route.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
-#auto-git v6.2
+# auto-git v6.7
+# 2022-2023 @arfazhxss
+
+unEnScore=50
+unEnString=$(printf '_%.0s' $(seq 1 "$unEnScore"))
+
+function pushChanges() {
+  git add . && \
+  git add -u && \
+  git commit -m $"$commt"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+  git push origin HEAD
+  git log > commit-hist.txt
+  rm -Rf .DS_Store/
+  echo -e "${unEnString}\n\t\tYour changes have been pushed\n\t\tto the repository :)\n${unEnString}"
+}
 
 find . -name ".DS_Store" -type f -delete
 echo -e ".DS_Store\ncommit-hist.txt" > .gitignore
-
-echo -e "\n_______________________________________________________\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
+echo -e "\n${unEnString}\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
 read -s -n 3 -p "(yes/ENTER): " answer
 
 if [[ $answer == "yes" || $answer == "Yes" || $answer == "YES" ]]; then
-  echo -e "YES'ED\n-------------------------------------------------------"
+  echo -e "YES'ED\n${unEnString}"
   git stash
   git stash clear
-  git pull origin main
-  echo -e "-------------------------------------------------------\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n_______________________________________________________"
+  git pull
+  echo -e "${unEnString}\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n${unEnString}"
 else
-  echo -e "ENTER'ED\n-------------------------------------------------------"
+  echo -e "ENTER'ED\n${unEnString}"
   read -p "Your Commit Message: " commt
 
   if [[ -z "$commt" || ${#commt} -lt 3 ]]; then
     commt="Routine Commit"
-    git add . && \
-    git add -u && \
-    git commit -m $"$commt"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-    git push origin HEAD
-    git log > commit-hist.txt
-    rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    pushChanges
   else
-    git add . && \
-    git add -u && \
-    git commit -m $"$commt"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-    git push origin HEAD
-    git log > commit-hist.txt
-    rm -Rf .DS_Store/
-    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+    pushChanges
   fi
 fi
